8a14992231d055f1c5e2bc5094a6f3a19cbd1bb9
Luiz Augusto Santana <luizsantossantana@outlook.com>
2018-09-01 00:51:39 -0300

    JavaScript aprimorado

--- js/messages.js
+++ js/messages.js
@@ -6,24 +6,7 @@ $(document).ready(function() {
 
     $("#message-input").keypress(function(e) {
         if ((e.which == 13 || e.keycode == 13) && $("#message-input").val() != "") {
-            if (me == true) {
-                if (lastme == true) {
-                    $(".message").last().removeClass("tail")
-                    $("#message-flow").append("<div class='d-flex flex-row justify-content-end'><div class='message out tail text shadow'>" + $("#message-input").val() + "</div></div>")
-                } else {
-                    $("#message-flow").append("<div class='d-flex flex-row justify-content-end'><div class='message out head tail text shadow'>" + $("#message-input").val() + "</div></div>")
-                    lastme = true;
-                }
-            } else {
-                if (lastme == true) {
-                    $("#message-flow").append("<div class='d-flex flex-row justify-content-start'><div class='message in head tail text shadow'>" + $("#message-input").val() + "</div></div>")
-                    lastme = false;
-                } else {
-                    $(".message").last().removeClass("tail")
-                    $("#message-flow").append("<div class='d-flex flex-row justify-content-start'><div class='message in tail text shadow'>" + $("#message-input").val() + "</div></div>")
-                }
-            }
-            $("#chat").scrollTop($("#message-flow").height());
+            $(this).displayMessage("t", $("#message-input").val());
         }
     });
 
@@ -32,46 +15,75 @@ $(document).ready(function() {
     });
 
     $("input[type=file]").change(function() {
-        if (me == true) {
-            if (lastme == true) {
-                $(".message").last().removeClass("tail")
-                $("#message-flow").append("<div class='d-flex flex-row justify-content-end'><div class='message out tail shadow'><img class='img' src='' ></div></div>")
-            } else {
-                $("#message-flow").append("<div class='d-flex flex-row justify-content-end'><div class='message out head tail shadow'><img class='img' src='' ></div></div>");
-                lastme = true;
-            }
-        } else {
-            if (lastme == true) {
-                $("#message-flow").append("<div class='d-flex flex-row justify-content-start'><div class='message in head tail shadow'><img class='img src='' ></div></div>")
-                lastme = false;
-            } else {
-                $(".message").last().removeClass("tail")
-                $("#message-flow").append("<div class='d-flex flex-row justify-content-start'><div class='message in tail shadow'><img class='img src='' ></div></div>")
-            }
-
-        }
         var file = document.querySelector('input[type=file]').files[0];
-        var reader = new FileReader();
-        reader.onloadend = function() {
-            $(".img").last().attr("src", reader.result);
-        }
-        if (file) {
-            reader.readAsDataURL(file);
-        } else {
-            $(".img").last().attr("src", "");
-        }
-        $("input[type=file]").val("");
-        $(".img").on('load', function() {
-            $("#chat").scrollTop($("#message-flow").height());
-
-        });
+        $(this).displayMessage("i", file);
+        $(this).val("");
     })
 
 });
 
-(function($) {
-    $.fn.displayMessage = function() {
+jQuery.fn.extend({
+    displayMessage: function(a, content) {
+        $("#message-flow").append("<div class='d-flex flex-row'>");
+        $("#message-flow .flex-row").last().append("<div class='message shadow'>");
+        if (me === true) {
+            $("#message-flow .flex-row").last().addClass("justify-content-end");
+            if (lastme === true) {
+                $(".message").eq(-2).removeClass("tail");
+                $(".message").last().addClass("message out tail text");
+            } else {
+                $(".message").last().addClass("message head out tail");
+                lastme = true;
+            }
+        } else {
+            $("#message-flow .flex-row").last().addClass("justify-content-start");
+            if (lastme === true) {
+                $(".message").last().addClass("message in head tail");
+                lastme = false;
+            } else {
+                $(".message").eq(-2).removeClass("tail");
+                $(".message").last().addClass("message in tail");
+            }
+        }
+        switch (a) {
+            case "t":
+                $(".message").last().addClass("text").append(content);
+                break;
+            case "i":
+                $(".message").last().append("<img class='img' src=''>");
+                File.imageConver(content);
+                $(".message .img").on("load", function() {
+                    $("#chat").scrollTop($("#message-flow").height());
+                });
+        }
+        $(".message").last().append("<small class='d-flex flex-row justify-content-end p-1'>");
+        $(".message small").last().append($(this).getTime());
+        $("#chat").scrollTop($("#message-flow").height());
+    }
+});
 
-        return this;
-    };
-})(jQuery);
+
+File = {
+    imageConver: function(file) {
+        var reader = new FileReader();
+        reader.onloadend = function() {
+            $(".message .img").last().attr("src", reader.result);
+        }
+        reader.readAsDataURL(file);
+    }
+};
+
+jQuery.fn.extend({
+    getTime: function() {
+        var d = new Date($.now());
+        var m = d.getMinutes();
+        var h = d.getHours();
+        if (h < 10) {
+            h = "0" + h;
+        }
+        if (m < 10) {
+            m = "0" + m;
+        }
+        return h + ":" + m;
+    }
+});
