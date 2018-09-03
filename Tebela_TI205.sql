282be19888fc864cd3ad094bda1663bdb44a635b
SmartRabbit42 <39036110+SmartRabbit42@users.noreply.github.com>
2018-09-03 10:02:40 -0300

    Update Tebela_TI205.sql

--- Tebela_TI205.sql
+++ Tebela_TI205.sql
@@ -5,10 +5,15 @@ create table Rooms(
 	time datetime not null
 )
 
+drop table Users
+drop table RoomUser
+drop table Mensagens
+
 create table Users(
 	codUser int primary key not null,
 	nickname varchar(30) not null,
-	password varchar(20) not null
+	passwordHash ntext not null,
+	salt ntext not null
 )
 
 create table RoomUser(
@@ -19,6 +24,16 @@ create table RoomUser(
 	constraint fkUser foreign key(codUser) references Users(codUser)
 )
 
+create table Mensagens(
+	codMessage int primary key not null,
+	messageHash ntext not null,
+	codUser int not null,
+	codRoom int not null,
+	constraint fkUser1 foreign key(codUser) references Users(codUser),
+	constraint fkRoom1 foreign key(codroom) references Rooms(codRoom)
+)
+
+select * from Mensagens
 select * from Users
 select * from Rooms
 select * from RoomUser
