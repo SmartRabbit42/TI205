a90cd2a4bedc23f741a7709e0d520c089100edec
luiz-santana <luizsantossantana@outlook.com>
2018-11-07 21:13:20 -0200

    interação com o banco de dados implementada

--- js/messages.js
+++ js/messages.js
@@ -1,4 +1,4 @@
-﻿"use strict";
+"use strict";
 
 
 var socket = io.connect("http://localhost:3000");
@@ -11,24 +11,24 @@ var ID = function() {
 };
 var username = "ANOM" + ID();
 
-function roomExists(room){
-	for(var i = 0; i < rooms.length; i++)
-		if(rooms[i] == room)
-			return i;
-	return -1;
+function roomExists(room) {
+  for (var i = 0; i < rooms.length; i++)
+    if (rooms[i] == room)
+      return i;
+  return -1;
 }
 
-function botoes(id){
-  for(var i = 0; i < rooms.length; i++){
-      $(".list-group-item")[i].style.backgroundColor = "#374557";
-  $(".list-group-item")[id].style.backgroundColor = "#0099cc";
+function botoes(id) {
+  for (var i = 0; i < rooms.length; i++) {
+    $(".list-group-item")[i].style.backgroundColor = "#374557";
+    $(".list-group-item")[id].style.backgroundColor = "#0099cc";
   }
 }
 //Login
-function enterRoom(e){
+function enterRoom(e) {
   var id = roomExists(e.currentTarget.getAttribute('id'));
   console.log(id);
-  if(id != -1){
+  if (id != -1) {
     $(".message-flow")[currentRoom].hidden = true;
     botoes(id);
     currentRoom = id;
@@ -36,93 +36,90 @@ function enterRoom(e){
   }
 }
 
-function createRoom(e){
+function createRoom(e) {
   if ($("#create-room-name").val() != "") {
-    socket.emit("create", {name: $("#create-room-name").val(), pass: $("#create-room-pass").val()});
-  }
-  else {
+    socket.emit("create", {
+      name: $("#create-room-name").val(),
+      pass: $("#create-room-pass").val()
+    });
+  } else {
     $(".c-n").append("<small class='text-danger'>Dê um nome à sua sala.");
   }
 }
 
-function joinRoom(a){
-  if(a == true){
+function joinRoom(a) {
+  if (a == true) {
     var pass = $("#enter-room-pass").val();
-    if(pass != ""){
-      socket.emit("join", {name: $("#enter-room .modal-title").html(), pass: pass});
+    if (pass != "") {
+      socket.emit("join", {
+        name: $("#enter-room .modal-title").html(),
+        pass: pass
+      });
     }
-  }
-  else{
-    socket.emit("join", {name:  a.currentTarget.id, pass: ""});
+  } else {
+    socket.emit("join", {
+      name: a.currentTarget.id,
+      pass: ""
+    });
     a.currentTarget.remove();
     $(this).addChat();
   }
 }
 
-
 function Register() {
-    var valido = true;
-    const name = new RegExp('^[A-Za-z]{3}.{0,17}');
-    const pass = new RegExp('.{8,30}')
-    if (!name.test($('#sig-user').val())) {
-        $(".r-n small").remove();
-        $(".r-n").append("<small class='text-danger'>Seu nome de usuário deve conter, no mínimo, 3 letras iniciais, seguidas ou não por outros 17 caracteres quaisquer.");
-        valido = false;
+  var valido = true;
+  const name = new RegExp('^[A-Za-z]{3}.{0,17}');
+  const pass = new RegExp('.{8,30}')
+  if (!name.test($('#sig-user').val())) {
+    $(".r-n small").remove();
+    $(".r-n").append("<small class='text-danger'>Seu nome de usuário deve conter, no mínimo, 3 letras iniciais, seguidas ou não por outros 17 caracteres quaisquer.");
+    valido = false;
+  } else {
+    $(".r-n small").remove();
+  }
+  if (!pass.test($('#sig-password').val())) {
+    $(".r-p small").remove();
+    $(".r-p").append("<small class='text-danger'>Sua senha deve conter, no mínimo, 8 letras, números ou <i>underlines</i>, sendo o máximo 30.");
+    valido = false;
+  } else {
+    $(".r-p small").remove();
+  }
+  if (!valido) {
+    return;
+  }
+  $.post("http://localhost:3000/register", $('#signup-form').serialize(), (data) => {
+    if (data.signup) {
+      $("#popup-reg .modal-body").html("<div class='h-2 text-success'>Registrado!");
+      setTimeout(function() {
+        $("#popup-reg").modal('hide');
+      }, 1500);
+      $('#signup-form').trigger("reset");
     } else {
-        $(".r-n small").remove();
+      $(".r-n small").remove();
+      $(".r-n").append("<small class='text-danger'>Nome de usuário já em uso.");
     }
-    if (!pass.test($('#sig-password').val())) {
-        $(".r-p small").remove();
-        $(".r-p").append("<small class='text-danger'>Sua senha deve conter, no mínimo, 8 letras, números ou <i>underlines</i>, sendo o máximo 30.");
-        valido = false;
-    } else {
-        $(".r-p small").remove();
-    }
-    if (!valido) {
-        return;
-    }
-    $.post("http://localhost:3000/register", $('#signup-form').serialize(), (data) => {
-        if (data.signup) {
-            $("#popup-reg .modal-body").html("<div class='h-2 text-success'>Registrado!");
-            setTimeout(function() {
-                $("#popup-reg").modal('hide');
-            }, 1500);
-            $('#signup-form').trigger("reset");
-        } else {
-            $(".r-n small").remove();
-            $(".r-n").append("<small class='text-danger'>Nome de usuário já em uso.");
-        }
-    });
+  });
 }
 
 function Login() {
-    $.post("http://localhost:3000/login", $('#login-form').serialize(), (data) => {
-        if (data.login) {
-            username = $("#log-user").val();
-            Logado(username);
-            $("#popup-log .modal-footer .h-3").remove();
-            $('#login-form').trigger("reset");
-            $("#popup-log").modal('hide');
-        } else {
-            $("#popup-log .modal-footer .text-danger").remove();
-            $("#popup-log .modal-footer").prepend("<div class='h-3 text-danger'>Nome de usuário ou senha incorreta.");
-            $("#log-password").val("");
-        }
-    });
+  socket.emit("logar", {
+    name: $("#log-user").val(),
+    pass: $("#log-password").val()
+  });
 }
 
 function Logado(username) {
-    $(".navbar-brand").html("Seja bem-vindo, " + username.bold());
-    $("#btn-reg").addClass("d-none");
-    $("#btn-log").addClass("d-none");
-    $("#btn-ext").removeClass("d-none");
+  $(".navbar-brand").html("Seja bem-vindo, " + username.bold());
+  $("#btn-reg").addClass("d-none");
+  $("#btn-log").addClass("d-none");
+  $("#btn-ext").removeClass("d-none");
 }
 //Socket
-socket.on("joined", function(data){
+socket.on("joined", function(data) {
   $($(".message-flow")[roomExists(data.room)]).append("<div style='color: red'>" + data.msg + "</div>");
 });
-socket.on("join", function(room){
-  if(currentRoom != -1)
+socket.on("join", function(room) {
+  if (currentRoom != -1)
     $(".message-flow")[currentRoom].hidden = true;
   currentRoom = rooms.length;
   rooms[currentRoom] = room;
@@ -130,26 +127,37 @@ socket.on("join", function(room){
   $("#room-joined .list-group").append("<li  class='list-group-item list-group-item-action' id='" + room + "' onclick='enterRoom(event)' style='background-color: #0099cc'><div class='d-flex flex-row justify-content-center'>" + room + "</div></li>");
   botoes(currentRoom);
 });
-socket.on("msg", function(data){
+socket.on("msg", function(data) {
   $(".message-flow")[findRoom(data.room)].innerHTML += data.msg;
 });
-socket.on("correctPass", function(data){
+socket.on("correctPass", function(data) {
   console.log(data);
-  if(data.response){
+  if (data.response) {
     $("#enter-room").modal('hide');
     $("#room-created #" + data.id).remove();
-    if(add == false)
+    if (add == false)
       $(this).addChat();
-  }
-  else {
+  } else {
     $("#enter-room .modal-footer .text-danger").remove();
     $("#enter-room .modal-footer").prepend("<div class='h-3 text-danger'>Senha incorreta.")
   }
 });
+socket.on("login", function(response) {
+  if (response) {
+    username = $("#log-user").val();
+    Logado(username);
+    $("#popup-log .modal-footer .h-3").remove();
+    $('#login-form').trigger("reset");
+    $("#popup-log").modal('hide');
+  } else {
+    $("#popup-log .modal-footer .text-danger").remove();
+    $("#popup-log .modal-footer").prepend("<div class='h-3 text-danger'>Nome de usuário ou senha incorreta.");
+    $("#log-password").val("");
+  }
+});
 
-
-socket.on("create", function(room){
-  if(currentRoom != -1){
+socket.on("create", function(room) {
+  if (currentRoom != -1) {
     $(".message-flow")[currentRoom].hidden = true;
   }
   currentRoom = rooms.length;
@@ -162,17 +170,19 @@ socket.on("create", function(room){
   botoes(currentRoom);
 });
 
-socket.on("created", function(data){
+socket.on("created", function(data) {
   if (data.pass) {
     $("#room-created .list-group").append("<li class='list-group-item list-group-item-action' id='" + data.name + "' data-toggle='modal' data-target='#enter-room' style='background-color: #18448e'><div class='d-flex flex-row justify-content-center'>" + data.name + "</div></li>");
-  }
-  else
+  } else
     $("#room-created .list-group").append("<li class='list-group-item list-group-item-action' id='" + data.name + "' onclick='joinRoom(event)' style='background-color: #18448e'><div class='d-flex flex-row justify-content-center' >" + data.name + "</div></li>");
 });
 
 $(document).ready(function() {
   socket.emit("login", username);
-  socket.emit("join", {name: "global", pass: ""});
+  socket.emit("join", {
+    name: "global",
+    pass: ""
+  });
 
   $("#enter-room").on("show.bs.modal", function(e) {
     console.log(e.relatedTarget.id);
@@ -180,17 +190,23 @@ $(document).ready(function() {
   })
 
   $("#create-room").submit(function(e) {
-      var roominfo = {name: $("#room-name").val(), pass: $("#room-pass").val()};
-      socket.emit("create", roominfo);
-      e.preventDefault();
+    var roominfo = {
+      name: $("#room-name").val(),
+      pass: $("#room-pass").val()
+    };
+    socket.emit("create", roominfo);
+    e.preventDefault();
   });
-  $("#room").click(function(){
+  $("#room").click(function() {
     $(".message-flow")[currentRoom].hidden = true;
     currentRoom = this.id;
     $(".message-flow")[currentRoom].hidden = false;
   });
-  $("#join-room").submit(function(){
-    var roominfo = {name: $("#room-name").val(), pass: $("#room-pass").val()};
+  $("#join-room").submit(function() {
+    var roominfo = {
+      name: $("#room-name").val(),
+      pass: $("#room-pass").val()
+    };
     socket.emit("join", roominfo);
   });
   $("#message-input").keypress(function(e) {
@@ -225,7 +241,7 @@ $(document).ready(function() {
 
   $("#add-chat").click($(this).addChat);
 
-  $('.modal').on('hidden.bs.modal', function(){
+  $('.modal').on('hidden.bs.modal', function() {
     $('.modal form').trigger("reset");
     $('.modal .text-danger').remove();
 
@@ -296,7 +312,10 @@ jQuery.fn.extend({
     }
     $("#chat").scrollTop($($(".message-flow")[currentRoom]).height());
 
-    var data = {msg: document.getElementsByClassName("message-flow")[currentRoom].lastChild.outerHTML, room: rooms[currentRoom]};
+    var data = {
+      msg: document.getElementsByClassName("message-flow")[currentRoom].lastChild.outerHTML,
+      room: rooms[currentRoom]
+    };
 
     socket.emit("msg", data);
   }
@@ -339,9 +358,9 @@ jQuery.fn.extend({
   }
 });
 
-function findRoom(room){
-  for(var i = 0; i < rooms.length; i++)
-    if(rooms[i] == room)
+function findRoom(room) {
+  for (var i = 0; i < rooms.length; i++)
+    if (rooms[i] == room)
       return i;
   return -1;
 }
--- js/server.js
+++ js/server.js
@@ -5,122 +5,212 @@ var cors = require('cors');
 var sha256 = require('js-sha256');
 var bodyParser = require('body-parser');
 var sql = require('mssql');
+var outrosql = require('mssql');
 var io = require('socket.io')(http);
 
 
-var clients = {};
-var rooms = [{name: "global", pass: ""}];
+
+var clients = [];
+var rooms = [{
+  name: "global",
+  pass: ""
+}];
+var roomClient = [];
 
 app.use(cors());
 
-sql.connect("Server=regulus.cotuca.unicamp.br;Database=PR118175;User Id=PR118175;Password=PR118175;");
-app.use(bodyParser.urlencoded({ extended: true }));
+
+
+
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 app.use(bodyParser.json());
 const rota = express.Router();
 app.use('/', rota);
 
-app.get('/', function(req, res){
-	res.send('Servidor Ativo');
+app.get('/', function(req, res) {
+  res.send('Servidor Ativo');
+});
+//Select nome, senha from Sala;      Select sala, usario from SalaUsuario
+http.listen(3000, async function() {
+  console.log("Servidor rodando na porta: 3000");
+  await sql.connect("Server=regulus.cotuca.unicamp.br;Database=PR118175;User Id=PR118175;Password=PR118175");
+  const request = new sql.Request();
+  request.query("Select userName as userName from Usuario").then(result => {
+      for (var i = 0; i < result.recordsets.length; i++) {
+        console.log(result.recordsets.length);
+        console.log(result.recordsets[0][i].userName);
+        //    rooms[i] = {
+        //      name: result.recordset[0][i].nome,
+        //      pass: result.recordset[0][i].senha
+        //    };
+        clients[i] = result.recordsets[0][i].userName;
+        //    roomClient[i] = {
+        //      name: clients[result.recordset[2].usuario],
+        //      room: rooms[result.recordset[2].sala].name
+      }
+      console.log(rooms + "\n \n" + clients + "\n \n" + roomClient);
+    }
+
+  ).catch(result => {
+    console.log(result);
+  });
 });
 
-http.listen(3000, function(){
-	console.log("Servidor rodando na porta: 3000")
-});
 
-io.on("connection", function(client){
-	console.log('usuario conectado');
-	client.on("login", function(username){
-		clients[client.id] = username;
-	});
-	client.on("join", function(data){
-		console.log(data);
-		var roomID = roomExists(data.name);
-		if(roomID != -1) {
-			if(rooms[roomID].pass == data.pass){
-				client.join(data.name);
-				client.emit("join", data.name);
-				console.log(clients[client.id] + " entrou na sala " + data.name);
-				client.emit("correctPass", {response: true, id: data.name});
-			 	client.emit("joined", {msg: "Você se conectou à sala", room: data.name});
-			  client.broadcast.to(data.name).emit("joined", {msg: clients[client.id] + " se conectou à sala", room: data.name});
-			}
-			else
-				client.emit("correctPass", {response: false, id: data.name});
-		}
-	});
-	client.on("msg", function(data){
-		if(roomExists(data.room) != -1){
-			client.broadcast.to(data.room).emit("msg", data);
-			const request = new sql.Request();
 
-			//request.query(`INSERT INTO Mensagem(sala, conteudo) VALUES('${data.room}', '${data.}')`);
-		}
-	})
-	client.on("create", function(data){
-		if(roomExists(data.name) == -1){
-			rooms[rooms.length] = {name: data.name, pass: data.pass};
-			client.join(data.name);
-			client.emit("create", data.name);
-			console.log("Usuario criou a sala " + data.name);
 
-			if(data.pass == "")
-				client.broadcast.emit("created", {user: clients[client.id], name: data.name, pass: false});
-			else
-				client.broadcast.emit("created", {user: clients[client.id], name: data.name, pass: true});
-		}
-		else
-			client.emit("roomExist", true);
-	});
-});
 
-function roomExists(room){
-	for(var i = 0; i < rooms.length; i++)
-		if(rooms[i].name == room)
-			return i;
-	return -1;
+io.on("connection", function(client) {
+  console.log('usuario conectado');
+  client.on("login", function(username) {
+    clients[client.id] = username;
+  });
+  client.on("join", function(data) {
+    var roomID = roomExists(data.name);
+    if (roomID != -1) {
+      if (rooms[roomID].pass == data.pass) {
+        client.join(data.name);
+        client.emit("join", data.name);
+        console.log(clients[client.id] + " entrou na sala " + data.name);
+        client.emit("correctPass", {
+          response: true,
+          id: data.name
+        });
+        client.emit("joined", {
+          msg: "Você se conectou à sala",
+          room: data.name
+        });
+        client.broadcast.to(data.name).emit("joined", {
+          msg: clients[client.id] + " se conectou à sala",
+          room: data.name
+        });
+        roomClient[roomClient.length] = {
+          room: data.name,
+          name: clients[client.id]
+        };
+      } else
+        client.emit("correctPass", {
+          response: false,
+          id: data.name
+        });
+    }
+  });
+  client.on("msg", function(data) {
+    if (roomExists(data.room) != -1) {
+      client.broadcast.to(data.room).emit("msg", data);
+      const request = new sql.Request();
+      request.query(`SELECT Sala.id AS salaId FROM Sala WHERE Sala.nome = '${data.room}'`).then(
+        result => {
+          request.query(`INSERT INTO Mensagem(sala, conteudo) VALUES ('${result.recordset[0].salaId}', '${data.msg}')`);
+        }
+      ).catch(result => {
+        console.log(result)
+      })
+      console.log(client.id);
+    }
+
+  });
+  client.on("create", function(data) {
+    if (roomExists(data.name) == -1) {
+      rooms[rooms.length] = {
+        name: data.name,
+        pass: sha256(data.pass)
+      };
+
+      const request = new sql.Request();
+      request.query(`INSERT INTO Sala(nome, senha) VALUES('${data.name}','${sha256(data.pass)}')`).then().catch(result => console.log(result));
+
+      client.join(data.name);
+      client.emit("create", data.name);
+      console.log("Usuario criou a sala " + data.name);
+      roomClient[roomClient.length] = {
+        room: data.name,
+        name: clients[client.id]
+      };
+
+      if (data.pass == "")
+        client.broadcast.emit("created", {
+          user: clients[client.id],
+          name: data.name,
+          pass: false
+        });
+      else
+        client.broadcast.emit("created", {
+          user: clients[client.id],
+          name: data.name,
+          pass: true
+        });
+    } else
+      client.emit("roomExist", true);
+  });
+
+  client.on("logar", function(data) {
+    const name = data.name;
+    const pass = sha256(data.pass);
+    const request = new sql.Request();
+    request.query(`SELECT CAST(CASE WHEN EXISTS(SELECT * FROM Usuario WHERE username = '${name}' AND userpass = '${pass}') THEN 1 ELSE 0 END AS BIT) AS re`).then(
+      result => {
+        if (result.recordset[0].re == true) {
+          client.emit("login", true);
+          for (var i = 0; i < roomClient.length; i++) {
+            console.log(client.id);
+            if (roomClient[i].name == name) {
+              client.join(roomClient[i].room);
+              client.emit("joinar", roomClient[i].room);
+            }
+          }
+        } else {
+          client.emit("login", false);
+        }
+      }).catch(result => {
+      console.log(result)
+    })
+  });
+})
+
+function roomExists(room) {
+  for (var i = 0; i < rooms.length; i++)
+    if (rooms[i].name == room)
+      return i;
+  return -1;
 }
 
 rota.post('/register', (req, res) => {
-    res.header("Access-Control-Allow-Origin", "*");
-    const name = req.body.name;
-    const age = req.body.age;
-    const pass = sha256(req.body.pass);
-    const request = new sql.Request();
-    request.query(`SELECT CAST(CASE WHEN EXISTS(SELECT * FROM Usuario WHERE username = '${name}') THEN 1 ELSE 0 END AS BIT) AS re`).then(
-        result => {
-            if (result.recordset[0].re == true) {
-                res.json({ signup: false });
-            } else {
-                request.query(`INSERT INTO Usuario(username, userage, userpass) VALUES('${name}', '${age}', '${pass}')`);
-                res.json({ signup: true });
-            }
-        }).catch(result => { console.log(result) })
+  res.header("Access-Control-Allow-Origin", "*");
+  const name = req.body.name;
+  const age = req.body.age;
+  const pass = sha256(req.body.pass);
+  const request = new sql.Request();
+  request.query(`SELECT CAST(CASE WHEN EXISTS(SELECT * FROM Usuario WHERE username = '${name}') THEN 1 ELSE 0 END AS BIT) AS re`).then(
+    result => {
+      if (result.recordset[0].re == true) {
+        res.json({
+          signup: false
+        });
+      } else {
+        request.query(`INSERT INTO Usuario(username, userage, userpass) VALUES('${name}', '${age}', '${pass}')`);
+        res.json({
+          signup: true
+        });
+      }
+    }).catch(result => {
+    console.log(result)
+  })
 })
 
-rota.post('/login', (req, res) => {
-    res.header("Access-Control-Allow-Origin", "*");
-    const name = req.body.name;
-    const pass = sha256(req.body.pass);
-    const request = new sql.Request();
-    request.query(`SELECT CAST(CASE WHEN EXISTS(SELECT * FROM Usuario WHERE username = '${name}' AND userpass = '${pass}') THEN 1 ELSE 0 END AS BIT) AS re`).then(
-        result => {
-            if (result.recordset[0].re == true) {
-                res.json({ login: true });
-            } else {
-                res.json({ login: false });
-            }
-        }).catch(result => { console.log(result) })
-})
 
-rota.post('/media', (req, res) => {
-    res.header("Access-Control-Allow-Origin", "*");
-    console.log(req.body)
-    var form = new formidable.IncomingForm();
-    form.parse(req);
-    form.on('fileBegin', (name, file) => {
-    	file.path = "C:\\Users\\Luiz\\Desktop" + '/uploads/' + file.name;
-    	console.log(file.name)
-    });
-    form.on('file', (name, file) => console.log('Uploaded ' + file.name));
-    res.sendStatus(200);
-})
+
+/*rota.post('/media', (req, res) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  console.log(req.body)
+  var form = new formidable.IncomingForm();
+  form.parse(req);
+  form.on('fileBegin', (name, file) => {
+    file.path = "C:\\Users\\Luiz\\Desktop" + '/uploads/' + file.name;
+    console.log(file.name)
+  });
+  form.on('file', (name, file) => console.log('Uploaded ' + file.name));
+  res.sendStatus(200);
+})*/
