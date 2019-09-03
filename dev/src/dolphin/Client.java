5854c7eb84e2e8605892bc4dcad1b4f1675f4bb5
GABRIEL LUIZ ADRIAO PORTELA DE OLIVEIRA <u18175@academico.cotuca.unicamp.br>
2019-09-03 11:42:23 -0300

    Initialized client UA

--- dev/src/dolphin/Client.java
+++ dev/src/dolphin/Client.java
@@ -1,12 +1,26 @@
 package dolphin;
 
+import java.awt.Container;
+import java.awt.Dimension;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
 
+import java.awt.Color;
+import javax.swing.JPanel;
+import java.awt.BorderLayout;
+import java.awt.CardLayout;
+import java.awt.Component;
+import javax.swing.Box;
+import javax.swing.BoxLayout;
+
 public class Client {
 
-	private JFrame frame;
+	private JFrame frmDolphin;
+	
+	private JPanel activePanel;
+	
+	private final Dimension expectedDimension = new Dimension(500, 600);
 
 	/**
 	 * Launch the application.
@@ -16,7 +30,7 @@ public class Client {
 			public void run() {
 				try {
 					Client window = new Client();
-					window.frame.setVisible(true);
+					window.frmDolphin.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
@@ -35,9 +49,54 @@ public class Client {
 	 * Initialize the contents of the frame.
 	 */
 	private void initialize() {
-		frame = new JFrame();
-		frame.setBounds(100, 100, 450, 300);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frmDolphin = new JFrame();
+		frmDolphin.setTitle("dolphin");
+		frmDolphin.setBounds(100, 100, 500, 600);
+		frmDolphin.setMinimumSize(expectedDimension);
+		frmDolphin.setExtendedState(JFrame.MAXIMIZED_BOTH); 
+		frmDolphin.setVisible(true);
+		frmDolphin.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		Container contentPane = frmDolphin.getContentPane();
+		contentPane.setLayout(new CardLayout(0, 0));
+		
+		
+		// Entry
+		JPanel panEntry = new JPanel();
+		panEntry.setBackground(new Color(15, 100, 150, 255));
+		panEntry.setLayout(new BorderLayout(0, 0));
+		
+		Box box = new Box(BoxLayout.Y_AXIS);
+		
+		JPanel panAuthentication = new JPanel();
+		panAuthentication.setBackground(new Color(10, 50, 75, 255));
+		panAuthentication.setPreferredSize(expectedDimension);
+		panAuthentication.setMaximumSize(expectedDimension);
+		panAuthentication.setMinimumSize(expectedDimension);
+		
+		
+		
+		
+		box.add(Box.createVerticalGlue());
+		box.add(panAuthentication);
+		box.add(Box.createVerticalGlue());
+		
+		panEntry.add(box);
+		
+		frmDolphin.getContentPane().add(panEntry, "authentication");
+		
+		
+		// Master
+		JPanel panMaster = new JPanel();
+		panMaster.setBackground(new Color(15, 100, 150, 255));
+		panMaster.setLayout(new BorderLayout(0, 0));
+		
+		frmDolphin.getContentPane().add(panMaster, "master");
+		
+		
+		// Start up
+		activePanel = panEntry;
+		
+		// Events
 	}
-
 }
